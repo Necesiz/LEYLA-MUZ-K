64fa0f592b63bcc5aeab2d2f2efc912cd2536aed
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-03-18 02:06:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "AgB0-_NdUZX9F8r0fDpxM8IknD-cxQTTU6BYTkNS5Lw42dxa4pTCuPlrG-kU6yBwwcUTGl8Z1Ejc5HXiOo9H-_jNsmxhz_SyvIOLplNdZGi7aI0ZkOcP4rp-yMUtMBYL9l9IOCA54_nSrhpPwdG6672fnn5TlhZMibVh79qXRS5VpcycHIOSa-87ckXql_LVZKhGh0w_wDeppRSYYcewgUrMUlilT6AJ4jvBbppb_s0Uz2N1pxXSFtWa6GYUhl7oUuu3ahfRxdb9Iboh_prlD0s637sJTBdYbP61t8NogvWjlWdCNk4RzIuH7ts7ox5mNXvCI0zwYkY_gWh5bQR-xMDmAAAAAT8q85oA")
-BOT_TOKEN = getenv("BOT_TOKEN", "5842321851:AAF4iPzvy8uI8oz7mj2T8YSTxSHJ8z6SAhc")
-BOT_NAME = getenv("BOT_NAME", "LEYLA") 
-API_ID = int(getenv("API_ID", "17568815"))
-API_HASH = getenv("API_HASH", "177622d39f23e7c3d015f3d6ebaacd31")
-BOT_USERNAME = getenv("BOT_USERNAME", "leylamusiciBot")
+SESSION_NAME = getenv("SESSION_NAME", "AgAj0wFPavV5LhgeeK2gu7LiX3bLApjj9Oq42JAQOUodAtPgR_o0DXbd0RPKm0Jsxz5sw49B3hupQyS4OfFwbzbff_BLQb7gEEywlosiHsVELhYnbmhgXIA8QdvwP6lg_QUqx4Eflowz6fxSwpeDKMNCpxNIDXSkgeNr2owRlLJaeGDbyLZFB26_QZEgIus2wkYg6sYayCqzrzYSPegLEybUuilod_6qbaS8D9dXDWVzWRAmM-2PNfQcmqB3kSOqCgDzLI6Y1pRH37VpwAFp2SbiOTX1Jm_E5Mo0CuSbPRQnTMtM5rmvOqSsGeqVg0lZCJajFCCZve_C7CM6umBjfo6FAAAAAUhXc-UA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5681957815:AAFu8TdIl9pYlQjSzm2r8rDL1TQqjxGyzGg")
+BOT_NAME = getenv("BOT_NAME", "AzeSongRobot") 
+API_ID = int(getenv("API_ID", "15954332"))
+API_HASH = getenv("API_HASH", "85adea6f1eaf068b707703b4846a9ced")
+BOT_USERNAME = getenv("BOT_USERNAME", "AzeSongRobot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5508658149").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5134595693").split()))
